9eb721ab928329ed1e5d0136595ab089da42bb85
Mateus Rodrigues <devrdgs@gmail.com>
2020-03-10 13:36:51 -0300

    feat: created datepicker related constants

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+import { WeekDay } from "../interfaces";
+
+export const weekDays: WeekDay[] = [
+  { label: "Sun", value: "Sunday" },
+  { label: "Mon", value: "Monday" },
+  { label: "Tue", value: "Tuesday" },
+  { label: "Wed", value: "Wednesday" },
+  { label: "Thu", value: "Thursday" },
+  { label: "Fri", value: "Friday" },
+  { label: "Sat", value: "Saturday" }
+];
+
+export const months: string[] = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
