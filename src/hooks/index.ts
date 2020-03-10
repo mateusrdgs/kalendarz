b441985fc4e321c38ccd3a58279813bfbc2a2d06
Mateus Rodrigues <devrdgs@gmail.com>
2020-03-10 13:37:08 -0300

    feat: created datepicker related hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+export const useDate = (date: Date) => {
+  const selectedYear = date.getFullYear();
+  const selectedMonth = date.getMonth();
+  const firstDateOfMonth = new Date(selectedYear, selectedMonth, 1);
+  const lastDateOfMonth = new Date(selectedYear, selectedMonth + 1, 0);
+
+  return { selectedYear, selectedMonth, firstDateOfMonth, lastDateOfMonth };
+};
